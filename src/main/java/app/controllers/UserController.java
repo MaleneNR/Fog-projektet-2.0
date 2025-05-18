c925989b9869ba737ea02a6555451f3bf1c99bb0
miekgram <cph-mg463@cphbusiness.dk>
2025-05-18 09:50:12 +0200

    fejltekst update

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -28,6 +28,8 @@ public class UserController {
         app.get("/login", ctx -> ctx.render("login.html"));
         app.post("/createUser", ctx -> createUser(ctx, connectionPool));
         app.post("/seForesporgsel", ctx -> AdminController.editProduct(ctx, connectionPool));
+        app.get("/index", ctx -> ctx.render("index.html"));
+
     }
 
     private static void createUser(@NotNull Context ctx, ConnectionPool connectionPool) {
@@ -64,12 +66,14 @@ public class UserController {
     private static void login(@NotNull Context ctx, ConnectionPool connectionPool) {
         String username= ctx.formParam("email");
         String password = ctx.formParam("password");
+
         try {
             User user = UserMapper.login(username, password, connectionPool);
             ctx.sessionAttribute("user", user);
             if(user.getRole() == 3 || user.getRole() == 2){
                 loginAdmin(ctx,connectionPool);
             } else{
+
 
                 if(ctx.sessionAttribute("length")==null || ctx.sessionAttribute("width")==null||ctx.sessionAttribute("height")== null){
                     throw new NullPointerException("Length, Width or Height is not set, prøv igen");
